649374f9a7a9474f201fc9e9b0cda0ffe6db52ac
Segun Bash <sunbash14@gmail.com>
2011-11-21 00:23:07 -0800

    Functional facebook connect sample app: check api keys

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facebookerapp::Application.config.secret_token = '8a8e49208085aa12213ddb7815ab9d2586f65748b24f8845254e77c702118c0bb714040492b2b1ee037d821540234601ca45080ec9a8faf89623c3818a76129c'
